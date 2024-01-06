6109e68f5a8580651a7ae79c398c8329fd82588a
«Alena» <mrk.alena@yandex.ru>
2024-01-06 17:13:49 +1000

    новые корректировки в введенных данных

--- Program.cs
+++ Program.cs
@@ -1,14 +1,12 @@
-// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*
-//Примеры:  
-//  [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]  
-//  [“1234”, “1567”, “-2”, “computer science”] → [“-2”]  
-//  [“Russia”, “Denmark”, “Kazan”] → []
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+//либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.*
+//Примеры:  [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]  [“1234”, “1567”, “-2”, “computer science”] → [“-2”] [“Russia”, “Denmark”, “Kazan”] → []
 
-Console.clear();
+Console.Clear();
 int n = GetNumberFromUser($"Please, enter the number of array elements: ", "Input error!"); //вызов метода запроса числа у пользователя
 string[] array = GetArray(n);                                                               //вызов метода создания первоначального массива
 Console.Write($"[{String.Join(",", array)}] -> ");                                          //вывод первоначального массива
-FinalArray(array);                                                                          //вызов финального метода задачи
+FinalArray(array);                                                                        //вызов финального метода задачи
 // конец программы
 
 // Определение методов
@@ -22,7 +20,7 @@ int GetNumberFromUser(string message, string errorMessage)
         bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
         if (isCorrect)
             return Math.Abs(userNumber);
-            Console.WriteLine(errorMessage);
+        Console.WriteLine(errorMessage);
     }
 }
 //2. Метод создания первоначального массива
@@ -46,13 +44,13 @@ void FinalArray(string[] Arr)
 {
     if (Arr[i].Lenght <= 5) size++;
 }
-string[] final = new string[size];
+string[] final = new String[size];
 for (int j = 0; j < Arr.Lenght; j++)
 {
     if (Arr[j].Lenght <= 5)
     {
         final[k] = Arr[j];
-        k++
+        k++;
     }
 }
 Console.Write($"[{String.Join(",", final)}]");
